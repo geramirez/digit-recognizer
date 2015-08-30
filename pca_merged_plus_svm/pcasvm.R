f55090927fcb410b9412deef16c613a80002c73f
unknown <samarths@MININT-APFD07T.redmond.corp.microsoft.com>
2015-08-30 15:39:33 -0700

    Merge training and test sets, run PCA on merged sets, apply PCA to per-digit training and test sets, finally use SVM for predictions.

---
+++ pca_merged_plus_svm/pcasvm.R
@@ -0,0 +1,50 @@
+dataSetsPath <- "C:\\Users\\samarths\\Desktop\\newdata\\"
+
+require(e1071)
+svm.models <- list()
+
+parentTestSet <- read.csv(paste(dataSetsPath, "test_FS.csv", sep=''), header = TRUE)
+parentTrainingSet <- read.csv(paste(dataSetsPath, "train_FS.csv", sep=''), header=TRUE)
+parentTrainingSet <- parentTrainingSet[,-1]
+mergedSet <- rbind(parentTrainingSet, parentTestSet)
+
+# PCA on merged set.
+pca <- prcomp(mergedSet, scale = TRUE)
+
+# Find 95% threshold of cumulative variance proportion of the principal components.
+thresh <- max(which(cumsum(pca$sdev^2 / sum(pca$sdev^2)) <= 0.95))
+
+# for each digit
+for (i in 0:9) {
+  print(i)
+  trainingSet <- read.csv(paste(dataSetsPath, "Train_FS_scaled\\", "trainFS-digit", i, ".csv", sep=''), header=TRUE)
+
+  # Apply PCA to training set.
+  train.pca <- predict(pca, trainingSet[,-ncol(trainingSet)])
+  
+  # Train SVM model.
+  model <- svm(train.pca[,1:thresh], as.factor(trainingSet[,ncol(trainingSet)]), probability = TRUE)
+
+  # Add the SVM model to the list of models, one per digit.
+  svm.models[[length(svm.models)+1]] <- model
+}
+
+testSet <- read.csv(paste(dataSetsPath, "test_FS.csv", sep=''), header = TRUE)
+# apply PCA to test set
+test.pca <- predict(pca, testSet)
+
+predictions <- matrix(nrow = nrow(testSet), ncol=0)
+
+# for each digit
+for (i in 0:9) {
+  print(i)
+  
+  # Predictions from SVM for this digit.
+  prediction <- predict(svm.models[[i+1]], test.pca[,1:thresh], probability = TRUE)
+  
+  # Store predicted probability for this digit.
+  predictions <- cbind(predictions, attr(prediction, "probabilities")[,"1"])
+}
+
+require(MASS)
+write.matrix(predictions, paste(dataSetsPath, "probabilities.csv", sep=''), sep=',')
